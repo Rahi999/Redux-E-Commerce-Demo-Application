dab98ba70efa45fa9d1961f00d80dff5025d0dcc
Rahim Ansari <rahimansari4560@gmail.com>
2022-11-08 05:58:29 +0530

    Functioa exported For Access LocalStorage Data

---
+++ src/utils/accessLocalStorage.js
@@ -0,0 +1,18 @@
+// for Get data from LocalStorage
+
+function loadData(key) {
+    try {
+      let temp = localStorage.getItem(key);
+      temp = JSON.parse(temp);
+      return temp;
+    } catch (e) {
+      return undefined;
+    }
+  }
+  
+  function saveData(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+  }
+  
+  export { loadData, saveData };
+  
