1aca1112e4999aac2c14ecd10e86107c2f494b8f
Rahim Ansari <rahimansari4560@gmail.com>
2022-11-08 05:54:57 +0530

    Landing Page Added

---
+++ src/Ui.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link, Route, Router } from "react-router-dom";
+import Home from "./components/Home";
+
+const Ui = () => {
+  return (
+    <>
+      <div>
+        <Home />
+      </div>
+    </>
+  );
+};
+export default Ui;
